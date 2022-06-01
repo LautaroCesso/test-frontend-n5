e0b1349727b84e7095b7599b7809cb7f3861616d
LautaroCesso <lautaro_cesso@hotmail.com>
2022-06-01 14:29:08 -0300

    Delete some comments

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { Route } from "wouter";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">My Ssr Frontend test</header>
       <Route path="/" component={MainPanel} />
       <Route path="/cart" component={CartPanel} />
     </div>
